770b87a3a86a64d7fa748869d005a1acfc21687f
Wesley Kirui <kkiruiwesley@gmail.com>
2020-08-20 00:46:39 +0300

    added email server acces

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret = os.getenv('CONSUMER_SECRET')
 access_token = os.getenv('ACCESS_TOKEN')
 access_secret = os.getenv('ACCESS_SECRET')
+
+# get email credential
+mail_username = os.getenv('MAIL_USERNAME')
+mail_password = os.getenv('MAIL_PASSWORD')
+mail_recipient = os.getenv('MAIL_RECIPIENT')
+mail_sender = os.getenv('MAIL_SENDER')
